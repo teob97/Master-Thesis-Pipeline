fed78f2e2686fa8b0daff9c584c92bf2b66b2577
Matteo Baratto <teo.baratto.97@gmail.com>
2022-12-09 21:06:40 +0100

    Added fgbuster functions

--- src/thesis.jl
+++ src/thesis.jl
@@ -9,22 +9,44 @@ import Stripeline as Sl
 export Instrument
 export get_single_map, get_foreground_maps, get_observations, get_white_noise
 export add_white_noise!
+export map2vec, fgbuster_basic_comp_sep
+
+# Define python functions ---------------------------------------------------------------------------
 
 # This function is automatically called when module is loaded
-# I define here all the python function that I need
 function __init__()
     py"""
     import pysm3
     import pysm3.units as u
+    
+    import pandas as pd
     import healpy as hp
+    
+    import fgbuster
 
     def pysm_sky_IQU(frequency, nside):
         sky = pysm3.Sky(nside=nside, preset_strings=["c1","d0","s0"])
         emission = sky.get_emission(frequency * u.GHz)
-        #emission = emission.to(u.uK_CMB, equivalencies=u.cmb_equivalencies(frequency * u.GHz))
+        emission = emission.to(u.uK_CMB, equivalencies=u.cmb_equivalencies(frequency * u.GHz))
         return pysm3.apply_smoothing_and_coord_transform(emission, rot=hp.Rotator(coord=("G", "C")))
+
+    def get_df(instruments):
+        buffer = []
+        for i in instruments:
+            buffer.append([i.frequency])       
+        df = pd.DataFrame(buffer, columns = ['frequency'])        
+        return df
+
+    def fgbuster_pipeline(data, instruments):
+        instrument = get_df(instruments)
+        # Componenti settate per le mappe in polarizzazione [NON SICURO SE LA FREQ DI RIFERIMENTO SIA GIUSTA]
+        components = [fgbuster.CMB(), fgbuster.Dust(353.), fgbuster.Synchrotron(23.)]
+        return fgbuster.basic_comp_sep(components, instrument, data[:,1:])
+
     """
 end
+
+#------------------------------------------------------------------------------------------------------
 
 Base.@kwdef struct Instrument
     name :: String = ""
@@ -68,6 +90,7 @@ function get_observations(
     return observations 
 end
 
+# Genera rumero bianco per una singola PolarizedHealpixMap
 function get_white_noise(nside, sigma)
 
     map = PolarizedHealpixMap{Float64, RingOrder}(nside)
@@ -86,13 +109,31 @@ end
 function add_white_noise!(signal, instrument)
 
     pixel_size = Healpix.nside2pixarea(signal.i.resolution.nside)
-    sigma = instrument.noisePerPixel * pixel_size * (180/π)^2
+    sigma = instrument.noisePerPixel * pixel_size * (180/π)^(2)
     noise = get_white_noise(signal.i.resolution.nside, sigma)
 
     signal.i = signal.i + noise.i
     signal.q = signal.q + noise.q
     signal.u = signal.u + noise.u
 
+end
+
+# Converte una lista di PolarizedHealpixMap in un vettore con row-major order,
+# conpatibile con Python ed fgbuster
+function map2vec(maps)
+    v = zeros(maps[1].i.resolution.numOfPixels, 3, length(maps))
+    for (index, m) in enumerate(maps)
+        v[:,1,index] = m.i.pixels
+        v[:,2,index] = m.q.pixels
+        v[:,3,index] = m.u.pixels
+    end
+    v[isnan.(v)] .= -1.6375e+30 # TROVARE UN MODO MIGLIORE USANDO IN PYTHON healpy.UNSEEN
+    return PyReverseDims(v)
+end
+
+function fgbuster_basic_comp_sep(maps, instruments)
+    data = map2vec(maps)
+    return py"fgbuster_pipeline"(data, instruments)    
 end
 
 end # module thesis
